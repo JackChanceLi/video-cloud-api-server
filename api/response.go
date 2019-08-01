c1dbb8198c3d7149253519eda07f7b3e654477fe
JianNan.Zheng <1023546080@qq.com>
2019-08-01 18:57:52 +0800

    http协议字段更新

--- api/response.go
+++ api/response.go
@@ -8,6 +8,15 @@ import (
 )
 
 func sendErrorResponse(w http.ResponseWriter, errResp defs.ErrorResponse) {
+	//允许跨域
+	w.Header().Set("Access-Control-Allow-Origin","*")
+	w.Header().Set("Access-Control-Allow-Credentials","true")
+	//允许请求类型
+	w.Header().Set("Access-Control-Allow-Methods","*")
+	//允许自定义头部
+	w.Header().Set("Access-Control-Allow-Headers","*")
+	w.Header().Set("Access-Control-Expose-Headers","*")
+
 	w.WriteHeader(200)
 
 	resStr, _ := json.Marshal(errResp)
@@ -15,6 +24,15 @@ func sendErrorResponse(w http.ResponseWriter, errResp defs.ErrorResponse) {
 }
 
 func sendNormalResponse(w http.ResponseWriter, resp string, sc int) {
+	//允许跨域
+	w.Header().Set("Access-Control-Allow-Origin","*")
+	w.Header().Set("Access-Control-Allow-Credentials","true")
+	//允许请求类型
+	w.Header().Set("Access-Control-Allow-Methods","*")
+	//允许自定义头部
+	w.Header().Set("Access-Control-Allow-Headers","*")
+	w.Header().Set("Access-Control-Expose-Headers","*")
+
 	w.WriteHeader(sc)
 	io.WriteString(w, resp)
 }
